c727662fe9808541109b418e5d712ba31002d459
Spycsh <757407490@qq.com>
2022-02-12 23:40:41 +0100

    add the connected components function

---
+++ src/main/java/io/github/spycsh/hesse/types/VertexComponentChange.java
@@ -0,0 +1,40 @@
+package io.github.spycsh.hesse.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class VertexComponentChange {
+    @JsonProperty("source")
+    private int source;
+
+    @JsonProperty("target")
+    private int target;
+
+    @JsonProperty("component_id")
+    private int componentId;
+
+    public VertexComponentChange() {
+        this(0, 0, 0);
+    }
+
+    private VertexComponentChange(int source, int target, int componentId){
+        this.source = source;
+        this.target = target;
+        this.componentId = componentId;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public int getComponentId() {
+        return componentId;
+    }
+
+    public static VertexComponentChange create(int source, int target, int componentId) {
+        return new VertexComponentChange(source, target, componentId);
+    }
+}
